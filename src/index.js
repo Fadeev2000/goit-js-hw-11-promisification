3a2d149c8607ca8045542e6c3bf226fea855d51f
Fadeev2000 <fda2000@ua.fm>
2021-05-16 00:06:54 +0300

    Добавил выполнение Задания 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
 import './sass/main.scss';
+
+// Задание 1
+const delay = ms => {
+    return new Promise(
+        resolve => setTimeout(() => resolve(ms), ms)
+    );
+};
+const logger = time => console.log(`Resolved after ${time}ms`);
+
+delay(2000).then(logger); // Resolved after 2000ms
+delay(1000).then(logger); // Resolved after 1000ms
+delay(1500).then(logger); // Resolved after 1500ms
+
+
+/*Тренировка Задание 1
+const delay = (ms, callNumber) => {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve({ ms, callNumber }), ms);
+    });
+};
+
+const logger = time => console.log(
+    `Resolved number ${time.callNumber} after ${time.ms}ms`
+);
+
+const initialCount = 1;
+const periodOfTime = 5000;
+function randomNumber() {
+    return Math.round(Math.random() * periodOfTime);
+}
+function makeCounter() {
+    let count = initialCount;
+
+    return function() {
+        return count++;
+    }
+}
+
+let counter = makeCounter();
+
+delay(randomNumber(), counter()).then(logger);
+delay(randomNumber(), counter()).then(logger); 
+delay(randomNumber(), counter()).then(logger); 
+delay(randomNumber(), counter()).then(logger); */
+
+
+//Задание 2
