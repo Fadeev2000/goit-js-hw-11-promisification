cedd41815be5a20be8de685bdb7426ec4ab2a947
Fadeev2000 <fda2000@ua.fm>
2021-05-18 00:09:42 +0300

    Добавил выполнение Задания 2 и 3 и разнес все задания в отдельные файлы

---
+++ src/js/index1.js
@@ -0,0 +1,42 @@
+const delay = ms => {
+    return new Promise(
+        resolve => setTimeout(() => resolve(ms), ms)
+    );
+};
+const logger = time => console.log(`Resolved after ${time}ms`);
+
+delay(2000).then(logger); // Resolved after 2000ms
+delay(1000).then(logger); // Resolved after 1000ms
+delay(1500).then(logger); // Resolved after 1500ms
+
+
+/*Тренировка Задание 1
+const delay = (ms, callNumber) => {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve({ ms, callNumber }), ms);
+    });
+};
+
+const logger = time => console.log(
+    `Resolved number ${time.callNumber} after ${time.ms}ms`
+);
+
+const initialCount = 1;
+const DELAY = 5000;
+function randomNumber() {
+    return Math.round(Math.random() * DELAY);
+}
+function makeCounter() {
+    let count = initialCount;
+
+    return function() {
+        return count++;
+    }
+}
+
+let counter = makeCounter();
+
+delay(randomNumber(), counter()).then(logger);
+delay(randomNumber(), counter()).then(logger); 
+delay(randomNumber(), counter()).then(logger); 
+delay(randomNumber(), counter()).then(logger); */
